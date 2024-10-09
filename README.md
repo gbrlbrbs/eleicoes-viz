aa5515c8ef52ba5eabc3739bd2579eb425facc5c
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2024-10-09 20:40:03 -0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Visualizações dos dados das eleições de 2024 em R
+
+Repositório para visualizar geograficamente os dados das eleições de 2024.
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Flake for reproducible environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { 
+          nativeBuildInputs = [ pkgs.bashInteractive ]; 
+          buildInputs = with pkgs; [ 
+            R rPackages.pagedown rPackages.rmarkdown rPackages.tidyverse
+            chromium 
+            pandoc 
+          ];
+        };
+      }
+    );
+}
